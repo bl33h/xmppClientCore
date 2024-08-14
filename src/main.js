e4bff70be3c4769ea2bcaa58f20ea58b342c2129
bl33h <per21371@uvg.edu.gt>
2024-08-13 23:40:45 -0600

    replies show up now

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { client, xml } = require('@xmpp/client');
 
+// create a new xmpp client
 const xmpp = client({
   service: process.env.SERVICE_URL,
   domain: process.env.DOMAIN,
@@ -9,6 +10,7 @@ const xmpp = client({
   password: process.env.PASSWORD
 });
 
+// connection
 xmpp.on('online', async (address) => {
   console.log('✔', 'connected as ->', address.toString());
 
@@ -17,17 +19,25 @@ xmpp.on('online', async (address) => {
 
   const message = xml(
     'message',
-    { type: 'chat', to: '@alumchat.lol' },
+    { type: 'chat', to: 'per21371@alumchat.lol' },
     xml('body', {}, 'hello hello')
   );
   console.log('message created:', message.toString());
 
   await xmpp.send(message);
   console.log('✔ message sent');
-
-  xmpp.stop();
 });
 
+// replies
+xmpp.on('stanza', (stanza) => {
+  if (stanza.is('message') && stanza.getChild('body')) {
+    const from = stanza.attrs.from;
+    const body = stanza.getChild('body').text();
+    console.log(`• you just got a message from [${from}]: ${body}`);
+  }
+});
+
+// error management
 xmpp.on('error', (err) => {
   console.error('!', 'connection error:', err);
 });
